017f3fe393ab7023888d0e4cc85f770d42445c06
Faith Musangi <faithmusangi50@gmail.com>
2025-03-12 20:31:35 +0300

    working with pusher

--- app/Events/MessageSent.php
+++ app/Events/MessageSent.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Events;
+use App\Models\Message;
 
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
@@ -9,6 +10,7 @@ use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
+
 
 class MessageSent implements ShouldBroadcast
 {
--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Message;
+use App\Events\MessageSent;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 
